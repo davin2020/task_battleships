5b44c3c23d0797643115437837a65df9550277ea
Davin <65677163+davin2020@users.noreply.github.com>
2023-07-01 00:57:22 +0100

    basic version with Task text hidden until clicked and random shuffling

--- script.js
+++ script.js
@@ -1,3 +1,92 @@
+
+
+let taskArray = [
+	"feed dog",
+	"dishes",
+	"finish report",
+	"laundry",
+	"commit code"
+]
+
+let itemClicked = document.getElementById("getTask");
+newRandomQuote.addEventListener("click", function() {
+	console.log('CLICKED get Task');
+  	// chooseRandomItem(randomQuotes);
+  	printTasks();
+  	// any benefit to adding all the code here?
+});
+
+function printTasks() {
+	shuffle(taskArray);
+	console.log("everyday im shuffling...")
+	let str =""
+	for (let index = 0; index < taskArray.length; index++) {
+			console.log("..printTask")
+		    console.log(taskArray[index]);
+		    let nameId = 'getTask' + index;
+
+		    // result += `<li>${item}</li>`;
+		    str += `<button class="button_ship" name="${nameId}" id="${nameId}" >blank</button>` ; 
+		    // str += '<button name="'nameId'" id="getTask' + index + '" >' + taskArray[index] + '</button>' ; 
+		    
+		    // str += '<button name="getTask' + index + '" id="getTask' + index + '" >' + taskArray[index] + '</button>' ; 
+		    // str += taskArray[index]
+		    // str += '<li>'+ slide + '</li>';
+		    // <button name="getTask" id="getTask" >taskArray[index]</button> 
+	}
+	// updateElementWithContent("userList", taskArray[index])
+	updateElementWithContent("allTasks", str)
+	//aboev sets value of string, only then can i add listeners!
+	addListeners();
+}
+
+// i dont want to hide the buttons, iwant to hide the words 
+function addListeners() {
+	for (let index = 0; index < taskArray.length; index++) {
+		let nameId = 'getTask' + index;
+		let item = document.getElementById(nameId);
+		// console.log(item)
+		// print(item) - this turns on browsers printing feature
+		item.addEventListener("click", function() {
+			console.log('added listener - task  name id ');
+			item.textContent = taskArray[index];
+			// item.hidden = false;
+			item.style.backgroundColor ="green"
+			//make things visible
+		  	// printTasks();
+		});
+	}
+}
+
+	
+// stole shuffling array from here - https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array#2450976
+// why doenst JS have built in shuffle function?
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+
+
+
+// ======
+
+
+
 // Customise this by adding in your own quotes
 let randomQuotes = [
 	"DeLorean Ipsum",
